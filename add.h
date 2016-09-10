cdbc4b03e5ad31b4e5113d60a57e4b4e19d36797
kewang3 <kewang3@aliyun.com>
2016-09-10 10:18:59 -0700

    add 2 .c files were add.c and add.h, and modify helloworld.c file

---
+++ add.h
@@ -0,0 +1,6 @@
+#ifndef _ADD_HEADER_FILE
+#define _ADD_HEADER_FILE
+
+extern int add(int a, int b);
+
+#endif
--- helloworld.c
+++ helloworld.c
@@ -1,21 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-int func(char* pStr)
-{
-	return 0;
-	return 1;
-	cout << "added new line" << endl;
-}
-
-void foo()
-{
-	cout << "i am c++" << endl;
-}
-
 int main() 
 {
-	printf("output is: %d\n", func(NULL));
-
 	return 0;
 }
