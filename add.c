f6b933cb0cc45392467e76c0f875098624e45bf6
kewang3 <kewang3@aliyun.com>
2016-09-11 09:07:39 -0700

    this is

--- add.c
+++ add.c
@@ -1,6 +1,14 @@
 #include "add.h"
 
-int add(int a, int b)
+int add_this_function(int e, int b)
 {
-    return a + b;
+    return a / c;
+	cout << "hello world" << endl;
+}
+
+//add some new line
+int foo()
+{
+    int i = 2;
+    cout << i << endl;
 }
--- sub.c
+++ sub.c
@@ -1,3 +1,9 @@
 #include "sub.h"
 
 extern int sub(int a, int b){return a - b;}
+
+this file is for test.
+
+how are you, fine thankyou andyou
+
+w
